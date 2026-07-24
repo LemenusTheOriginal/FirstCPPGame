677544827937eeefc9c09a3de51439ef448dbe43
Mike Kirillov <absovec@gmail.com>
2026-07-24 20:31:17 +0500

    imgui added

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
 	//std::cout << "Hello\n";
 
+	// Window
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE); // Make window resizable
 	InitWindow(800, 450, "Game Window"); // Create window
 	SetTargetFPS(30);
 
 	std::string dynamicText = "Press SPACE to change this text!";
 
+	// ImGUI
+# pragma region imgui
+	rlImGuiSetup(true);
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.FontGlobalScale = 2;
+#pragma endregion
+
 	//main loop
 	while (!WindowShouldClose())
 	{
+# pragma region imgui
+		rlImGuiBegin();
+
+		// ImGUI Docking
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		ImGui::PopStyleColor(2);
+#pragma endregion
+
 		if (IsKeyPressed(KEY_SPACE)) {
 			dynamicText = "Text changed successfully!";
 		}
@@ -27,9 +49,35 @@ int main()
 
 		DrawText(dynamicText.c_str(), 400, 200, 20, RED);
 
+# pragma region imgui windows
+		ImGui::Begin("test");
+		ImGui::Text("Hello");
+		if (ImGui::Button("Butt"))
+		{
+			std::cout << "Press\n";
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Button 2"))
+		{
+			std::cout << "You've got burrito\n";
+		}
+		ImGui::End();
+
+		ImGui::Begin("Second Window");
+		ImGui::Text("Bababooey");
+		ImGui::Separator();
+		ImGui::NewLine();
+		static float a = 0;
+		ImGui::SliderFloat("Slider", &a, 0, 1);
+		ImGui::End();
+#pragma endregion
+
+		rlImGuiEnd();
 		EndDrawing();
 	}
 
+
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;
