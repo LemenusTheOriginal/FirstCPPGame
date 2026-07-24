88e94799c5a9da1f73eb19fa137fc66c35e1b2ee
Mike Kirillov <absovec@gmail.com>
2026-07-24 14:53:28 +0500

    raylib added

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Hello\n";
+	//std::cout << "Hello\n";
 
-	// commit test
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE); // Make window resizable
+	InitWindow(800, 450, "Game Window"); // Create window
+	SetTargetFPS(30);
+
+	std::string dynamicText = "Press SPACE to change this text!";
+
+	//main loop
+	while (!WindowShouldClose())
+	{
+		if (IsKeyPressed(KEY_SPACE)) {
+			dynamicText = "Text changed successfully!";
+		}
+		else if (IsKeyReleased(KEY_SPACE))
+		{
+			dynamicText = "Press SPACE to change this text!";
+		}
+
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText(dynamicText.c_str(), 400, 200, 20, RED);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
